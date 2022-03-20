16f57a671a5dc4795d4693ed5ec0ac44041a3a28
Leonard Stutzer <leonard.stutzer@tink.com>
2022-03-20 02:14:23 +0100

    make pedersen go brum

--- pedersen.go
+++ pedersen.go
@@ -3,54 +3,122 @@ package pedersen
 import (
 	"crypto/sha256"
 	"fmt"
-	"github.com/pkg/errors"
+	"hash"
+	"log"
 	"math/big"
+
+	"github.com/pkg/errors"
 	"github.com/thecodingshrimp/pedersen-go/babyjub"
 )
 
-// var sha256Hash = sha256.New()
 const zokratesName = "test"
 
-func pedersenHashBasePoint(name string, i int) (*babyjub.Point, error) {
-	if i > 0xFFFF {
-		return nil, errors.New("Sequence number invalid")
-	}
-	if len(name) > 28 {
-		return nil, errors.New("Name too long")
-	}
-	// data = b"%-28s%04X" % (name, i)
-	formattedStr := fmt.Sprintf("%-28s%04X", name, i)
-	data := []byte(formattedStr)
-
-	sha256Hash := sha256.New()
-	_, err := sha256Hash.Write(data)
-	if err != nil {
-		return nil, err
-	}
-	hashed := sha256Hash.Sum(nil)
-	return babyjub.FromBytes(hashed)
-
+type PedersenHash struct {
+	name          string
+	segments      int
+	generators    []*babyjub.Point
+	isSized       bool
+	hasher256hash hasher
 }
 
-func pedersenHashWindows(name string, windows []byte) (*babyjub.Point, error) {
-	result := babyjub.Infinity()
+func New(name string, segments int) *PedersenHash {
+	ph := new(PedersenHash)
+	switch name {
+	case "":
+		ph.name = zokratesName
+	default:
+		ph.name = name
+	}
+
+	if segments > 0 {
+		ph.isSized = true
+		ph.segments = segments
+		ph.createGenerators()
+	}
+
+	ph.hasher256hash = makeHasher(sha256.New())
+
+	return ph
+}
+
+func (ph *PedersenHash) createGenerators() []*babyjub.Point {
+	ph.generators = make([]*babyjub.Point, ph.segments)
+	segments := ph.segments
 	var current *babyjub.Point
-	for j, window := range windows {
-		var err error
-		if j%62 == 0 {
-			current, err = pedersenHashBasePoint(name, j/62)
+	var err error
+	for i := 0; i < segments; i++ {
+		if i%62 == 0 {
+			current, err = ph.pedersenHashBasePoint(i / 62)
 			if err != nil {
-				return nil, err
+				log.Fatalf(err.Error())
+				return nil
 			}
 		}
-		j = j % 62
+		j := i % 62
 		if j != 0 {
 			current = babyjub.NewPoint().Add(current, current)
 			current = babyjub.NewPoint().Add(current, current)
 			current = babyjub.NewPoint().Add(current, current)
 			current = babyjub.NewPoint().Add(current, current)
 		}
-		segment := babyjub.NewPoint().Mul(big.NewInt(int64((window&0x3)+1)), current)
+		ph.generators[i] = current
+	}
+	return ph.generators
+}
+
+// https://github.com/ethereum/go-ethereum/blob/667e1c038e171fafb6e2136b02f8b01dd559cacf/consensus/ethash/algorithm.go#L95
+// hasher is a repetitive hasher allowing the same hash data structures to be
+// reused between hash runs instead of requiring new ones to be created.
+type hasher func(data []byte) []byte
+
+// https://github.com/ethereum/go-ethereum/blob/667e1c038e171fafb6e2136b02f8b01dd559cacf/consensus/ethash/algorithm.go#L100
+// makeHasher creates a repetitive hasher, allowing the same hash data structures to
+// be reused between hash runs instead of requiring new ones to be created. The returned
+// function is not thread safe!
+func makeHasher(h hash.Hash) hasher {
+	return func(data []byte) []byte {
+		h.Reset()
+		h.Write(data)
+		return h.Sum(nil)
+	}
+}
+
+func (ph *PedersenHash) pedersenHashBasePoint(i int) (*babyjub.Point, error) {
+	if i > 0xFFFF {
+		return nil, errors.New("Sequence number invalid")
+	}
+	if len(ph.name) > 28 {
+		return nil, errors.New("Name too long")
+	}
+	// data = b"%-28s%04X" % (name, i)
+	formattedStr := fmt.Sprintf("%-28s%04X", ph.name, i)
+	data := []byte(formattedStr)
+
+	return babyjub.FromBytes(ph.hasher256hash(data))
+}
+
+func (ph *PedersenHash) pedersenHashWindows(windows []byte) (*babyjub.Point, error) {
+	if !ph.isSized {
+		ph.segments = len(windows)
+		ph.isSized = true
+		ph.createGenerators()
+	}
+
+	if len(windows) > ph.segments {
+		errorMessage := fmt.Sprintf("Number of windows exceeds pedersenHasher config. %d vs. %d", len(windows), ph.segments)
+		log.Fatalf(errorMessage)
+		return nil, errors.New(errorMessage)
+	}
+
+	padding := (ph.segments - len(windows))
+	if padding > 0 {
+		windows = append(windows, make([]byte, padding)...)
+	}
+
+	result := babyjub.Infinity()
+	segment := babyjub.NewPoint()
+	for j, window := range windows {
+		segment = segment.Mul(big.NewInt(int64((window&0x3)+1)), ph.generators[j])
 		if window > 0x3 {
 			segment.X = segment.X.Neg(segment.X)
 		}
@@ -59,7 +127,7 @@ func pedersenHashWindows(name string, windows []byte) (*babyjub.Point, error) {
 	return result, nil
 }
 
-func PedersenHashBytes(name string, bytesArray ...[]byte) (*babyjub.Point, error) {
+func (ph *PedersenHash) PedersenHashBytes(bytesArray ...[]byte) (*babyjub.Point, error) {
 	bytes := []byte{}
 	for _, item := range bytesArray {
 		bytes = append(bytes, item...)
@@ -81,5 +149,5 @@ func PedersenHashBytes(name string, bytesArray ...[]byte) (*babyjub.Point, error
 		}
 		windows = append(windows, result)
 	}
-	return pedersenHashWindows(name, windows)
+	return ph.pedersenHashWindows(windows)
 }
--- pedersen_test.go
+++ pedersen_test.go
@@ -4,15 +4,17 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"testing"
+
 	"github.com/stretchr/testify/require"
 	"github.com/thecodingshrimp/pedersen-go/babyjub"
-	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPedersen_HashBytes(t *testing.T) {
-	point, err := PedersenHashBytes(zokratesName, []byte("abc"))
+	ph := New(zokratesName, 0)
+	point, err := ph.PedersenHashBytes([]byte("abc"))
 	assert.Nil(t, err)
 	expectedX := babyjub.NewIntFromString("9869277320722751484529016080276887338184240285836102740267608137843906399765")
 	expectedY := babyjub.NewIntFromString("19790690237145851554496394080496962351633528315779989340140084430077208474328")
@@ -23,7 +25,8 @@ func TestPedersen_HashBytes(t *testing.T) {
 }
 
 func TestPedersen_HashBytes2(t *testing.T) {
-	point, err := PedersenHashBytes(zokratesName, []byte("abcdefghijklmnopqrstuvwx"))
+	ph := New(zokratesName, 0)
+	point, err := ph.PedersenHashBytes([]byte("abcdefghijklmnopqrstuvwx"))
 	assert.Nil(t, err)
 	expectedX := babyjub.NewIntFromString("3966548799068703226441887746390766667253943354008248106643296790753369303077")
 	expectedY := babyjub.NewIntFromString("12849086395963202120677663823933219043387904870880733726805962981354278512988")
@@ -36,37 +39,39 @@ func TestPedersen_HashBytes2(t *testing.T) {
 // test case from zokrates_stdlib
 // https://github.com/Zokrates/ZoKrates/blob/master/zokrates_stdlib/tests/tests/hashes/pedersen/512bit.zok
 func TestPedersen_Zokrates(t *testing.T) {
+	ph := New(zokratesName, 0)
 	field512 := [512]byte{0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1}
 	bytes := bitsToBytes(field512[:])
 	// println(hex.EncodeToString(bytes))
-	point, err := PedersenHashBytes(zokratesName, bytes)
+	point, err := ph.PedersenHashBytes(bytes)
 	require.NoError(t, err)
 	// t.Log(point)
-	h1 := bytes32ToBits(PackPoint(point))
+	h1 := bytes32ToBits(Compress_Zokrates(point))
 	h0 := [256]byte{0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1}
 	require.Equal(t, h0[:], h1)
 }
 
 // generate new test cases for zokrates
 func TestPedersen_generate(t *testing.T) {
+	ph := New(zokratesName, 0)
 	eHex := "e24f1d03d1d81e94a099042736d40bd9681b867321443ff58a4568e274dbd83b"
 	eBytes, _ := hex.DecodeString(eHex)
 	eBits := bytesToBits(eBytes)
 	// hPx1 - pedersen hash x1
-	point, err := PedersenHashBytes(zokratesName, eBytes, eBytes)
+	point, err := ph.PedersenHashBytes(eBytes, eBytes)
 	require.NoError(t, err)
-	hPx1Bits := bytes32ToBits(PackPoint(point))
+	hPx1Bits := bytes32ToBits(Compress_Zokrates(point))
 	// hPx16, hPx32 - pedersen hash x16, x32
 	var hPx16Bits []byte
 	for i := 1; i < 32; i++ {
-		prev := PackPoint(point)
+		prev := Compress_Zokrates(point)
 		if i == 16 {
 			hPx16Bits = bytes32ToBits(prev)
 		}
-		point, err = PedersenHashBytes(zokratesName, prev[:], eBytes)
+		point, err = ph.PedersenHashBytes(prev[:], eBytes)
 		require.NoError(t, err)
 	}
-	hPx32Bits := bytes32ToBits(PackPoint(point))
+	hPx32Bits := bytes32ToBits(Compress_Zokrates(point))
 
 	// hSx1 - sha256 hash x1
 	prev := sha256.Sum256(eBytes)
